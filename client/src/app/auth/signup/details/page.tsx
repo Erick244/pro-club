aac5a08fd441ab92fb5b53e88482badb4984347a
Erick244 <erienrique03@gmail.com>
2024-08-19 18:03:33 -0300

    feat(auth/signup/details): add this page

---
+++ client/src/app/auth/signup/details/page.tsx
@@ -0,0 +1,36 @@
+import { IntroductionCardSection } from "@/components/auth/sections/IntroductionCardSection";
+import { IntroductionSection } from "@/components/auth/sections/IntroductionSection";
+import { H1 } from "@/components/typography/H1";
+import { Muted } from "@/components/typography/Muted";
+import { FormContainer } from "@/components/utils/forms/FormContainer";
+
+const introductionTexts = {
+    h1: "USERNAME",
+    muted: "Fill in a few more information's to complete your registration.",
+};
+
+export default function Page() {
+    return (
+        <div className="mt-5 space-y-10 lg:flex lg:justify-center lg:items-center lg:gap-10">
+            <IntroductionSection className="lg:hidden">
+                <H1>{introductionTexts.h1}</H1>
+                <Muted className="text-center text-xs">
+                    {introductionTexts.muted}
+                </Muted>
+            </IntroductionSection>
+
+            <IntroductionCardSection className="hidden lg:flex">
+                <H1 className="dark:text-background text-foreground">
+                    {introductionTexts.h1}
+                </H1>
+                <Muted className="text-center text-xs dark:text-background text-foreground">
+                    {introductionTexts.muted}
+                </Muted>
+            </IntroductionCardSection>
+
+            <FormContainer className="m-auto sm:min-h-[500px] sm:max-w-xl lg:max-w-2xl lg:w-full lg:m-0">
+                SIGNUP DETAILS FORM
+            </FormContainer>
+        </div>
+    );
+}
