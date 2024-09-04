71ee99f516eb239dca839ed50b4dd49e062c043c
Erick244 <erienrique03@gmail.com>
2024-09-04 16:50:54 -0300

    fix(CustomUserGradient): now this gradient is fixed on background

--- client/src/components/user/layout/CustomUserGradient.tsx
+++ client/src/components/user/layout/CustomUserGradient.tsx
@@ -13,7 +13,7 @@ export function CustomUserGradient() {
     return (
         <div
             style={{ background: customGradient }}
-            className="transition-all duration-300 -z-10 absolute h-screen w-screen top-0 left-0"
+            className="transition-all duration-300 -z-10 fixed min-h-screen w-screen inset-0"
         />
     );
 }
