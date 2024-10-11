c6c71e4b23228e39033d7c01c6c633e410f3ce3e
Erick244 <erienrique03@gmail.com>
2024-10-11 15:34:31 -0300

    fix(AppModule): Remove deleted controllers and providers imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
-import { AbcController } from './abc/abc.controller';
-import { AbcService } from './abc/abc.service';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
-    controllers: [AbcController],
-    providers: [AbcService],
 })
 export class AppModule {}
