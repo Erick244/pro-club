a59eade067fef5259a66b55ba183fedb43c4db6c
Erick244 <erienrique03@gmail.com>
2024-10-18 18:22:26 -0300

    feat(Server:SignUpRequestDto): add validations

--- server/src/auth/models/dtos/sign-up/sign-up-request.dto.ts
+++ server/src/auth/models/dtos/sign-up/sign-up-request.dto.ts
@@ -1,9 +1,21 @@
+import { IsEmail, IsNotEmpty, Length } from "class-validator";
+import { Match } from "../../../validators/match.validator";
+
 export class SignUpRequestDto {
+    @IsNotEmpty()
+    @Length(2, 50)
     name: string;
 
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
 
+    @IsNotEmpty()
+    @Length(8, 16)
     password: string;
 
+    @IsNotEmpty()
+    @Length(8, 16)
+    @Match("password", { message: "Passwords do not match" })
     confirmPassword: string;
 }
