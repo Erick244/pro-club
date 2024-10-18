b8a9de099b02aa2b9c56f48c792198658ed7c2a2
Erick244 <erienrique03@gmail.com>
2024-10-18 18:23:45 -0300

    feat(Server:SignInRequestDto): add validations

--- server/src/auth/models/dtos/sign-in/sign-in.request.dto.ts
+++ server/src/auth/models/dtos/sign-in/sign-in.request.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, Length } from "class-validator";
+
 export class SignInRequestDto {
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
 
+    @IsNotEmpty()
+    @Length(8, 16)
     password: string;
 }
