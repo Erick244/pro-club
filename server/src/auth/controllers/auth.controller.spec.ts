e7804c5c9558f9022cf2ec5a7b72f7a019e357f8
Erick244 <erienrique03@gmail.com>
2024-10-18 17:24:09 -0300

    feat(Server:AuthController): implemented signIn route

--- server/src/auth/controllers/auth.controller.spec.ts
+++ server/src/auth/controllers/auth.controller.spec.ts
@@ -1,4 +1,7 @@
 import { Test } from "@nestjs/testing";
+import { User } from "@prisma/client";
+import { SignInResponseDto } from "../models/dtos/sign-in/sign-in-response.dto";
+import { SignInRequestDto } from "../models/dtos/sign-in/sign-in.request.dto";
 import { SignUpRequestDto } from "../models/dtos/sign-up/sign-up-request.dto";
 import { SignUpResponseDto } from "../models/dtos/sign-up/sign-up-response.dto";
 import { AuthService } from "../services/auth.service";
@@ -9,6 +12,7 @@ describe("AuthController", () => {
     let authService: AuthService;
     const mockAuthService = {
         signUp: jest.fn(),
+        signIn: jest.fn(),
     };
 
     beforeEach(async () => {
@@ -59,6 +63,28 @@ describe("AuthController", () => {
                 responseDto,
             );
             expect(authService.signUp).toHaveBeenCalledWith(requestDto);
+        });
+    });
+
+    describe("sign in", () => {
+        it("should sign in user", async () => {
+            const requestDto: SignInRequestDto = {
+                email: "john.doe@example.com",
+                password: "password",
+            };
+            const responseDto: SignInResponseDto = {
+                user: {
+                    id: 123,
+                } as unknown as User,
+                authToken: "token",
+            };
+
+            jest.spyOn(mockAuthService, "signIn").mockReturnValue(responseDto);
+
+            expect(await controller.signIn(requestDto)).toStrictEqual(
+                responseDto,
+            );
+            expect(authService.signIn).toHaveBeenCalledWith(requestDto);
         });
     });
 });
--- server/src/auth/controllers/auth.controller.ts
+++ server/src/auth/controllers/auth.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { SignInResponseDto } from "../models/dtos/sign-in/sign-in-response.dto";
+import { SignInRequestDto } from "../models/dtos/sign-in/sign-in.request.dto";
 import { SignUpRequestDto } from "../models/dtos/sign-up/sign-up-request.dto";
 import { SignUpResponseDto } from "../models/dtos/sign-up/sign-up-response.dto";
 import { AuthService } from "./../services/auth.service";
@@ -10,5 +12,10 @@ export class AuthController {
     @Post("/signup")
     async signUp(@Body() dto: SignUpRequestDto): Promise<SignUpResponseDto> {
         return await this.authService.signUp(dto);
+    }
+
+    @Post("/signin")
+    async signIn(@Body() dto: SignInRequestDto): Promise<SignInResponseDto> {
+        return await this.authService.signIn(dto);
     }
 }
