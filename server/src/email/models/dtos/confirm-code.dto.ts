d9c77543e3bb1f9fbe2119604600186c54d0dce1
Erick244 <erienrique03@gmail.com>
2024-10-18 18:28:00 -0300

    feat(Server:ConfirmCodeDto): add validations

--- server/src/email/models/dtos/confirm-code.dto.ts
+++ server/src/email/models/dtos/confirm-code.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, Length } from "class-validator";
+
 export class ConfirmCodeDto {
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @Length(6, 6)
     code: string;
 }
