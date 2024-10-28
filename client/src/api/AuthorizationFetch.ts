108550222ed87d8f46a1115fb57934de98dcb7c3
Erick244 <erienrique03@gmail.com>
2024-10-28 16:53:30 -0300

    fix(Client:AuthorizationFetch): updated to use cookies names file

--- client/src/api/AuthorizationFetch.ts
+++ client/src/api/AuthorizationFetch.ts
@@ -1,10 +1,10 @@
 "use server";
 
-import { AUTH_TOKEN_NAME } from "@/constants";
+import { cookieNames } from "@/cookies/names";
 import { cookies } from "next/headers";
 
 export async function AuthorizationFetch(url: string, options?: RequestInit) {
-    const authToken = cookies().get(AUTH_TOKEN_NAME)?.value;
+    const authToken = cookies().get(cookieNames.AUTH_TOKEN)?.value;
 
     const resp = await fetch(url, {
         ...options,
