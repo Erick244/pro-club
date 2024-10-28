30aebf934daa78da4b5dc9bc8d29ef13d797a217
Erick244 <erienrique03@gmail.com>
2024-10-28 17:37:55 -0300

    feat(Client:functions/authorizations): added functions to allow users to access pages based on cookies

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,9 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Authorization } from "@/functions/authorizations";
 
 export default function Home() {
+    Authorization["/"]();
+
     return (
         <div>
             <ModeToggle />
---
+++ client/src/functions/authorizations.ts
@@ -0,0 +1,43 @@
+import { cookieNames } from "@/cookies/names";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+function existCookieOrRedirectTo(path: string, cookieKey: string) {
+    const cookie = cookies().get(cookieKey)?.value;
+
+    if (!cookie) {
+        redirect(path);
+    }
+}
+
+function notExistCookieOrRedirectTo(path: string, cookieKey: string) {
+    const cookie = cookies().get(cookieKey)?.value;
+
+    if (cookie) {
+        redirect(path);
+    }
+}
+
+function isFalseCookieOrRedirectTo(path: string, cookieKey: string) {
+    const cookie = cookies().get(cookieKey)?.value;
+
+    if (JSON.parse(cookie || "true")) {
+        redirect(path);
+    }
+}
+
+export const Authorization = {
+    "/": () => {
+        existCookieOrRedirectTo("/auth/signup", cookieNames.AUTH_TOKEN);
+
+        isFalseCookieOrRedirectTo(
+            "/auth/email-confirmation",
+            cookieNames.EMAIL_CONFIRMATION_PENDING
+        );
+
+        isFalseCookieOrRedirectTo(
+            "/auth/signup/details",
+            cookieNames.SIGN_UP_DETAILS_PENDING
+        );
+    },
+};
