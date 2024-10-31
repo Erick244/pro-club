68f1f6bb1e8b46b0b2928d7c20b6c15463344118
Erick244 <erienrique03@gmail.com>
2024-10-31 19:00:18 -0300

    feat(Client:useTimer): add a hook to management a time

---
+++ client/src/hooks/useTimer.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useTimer(startTime: number, localStorageKey: string) {
+    const [timer, setTimer] = useState<number>(startTime);
+
+    useEffect(() => {
+        const localStorageTimer = localStorage.getItem(localStorageKey);
+
+        if (localStorageTimer) {
+            setTimer(+localStorageTimer);
+        }
+    }, [localStorageKey]);
+
+    useEffect(() => {
+        const ONE_SECOND_IN_MS = 1000;
+
+        const interval = setInterval(() => {
+            setTimer(timer - 1);
+
+            localStorage.setItem(localStorageKey, JSON.stringify(timer));
+        }, ONE_SECOND_IN_MS);
+
+        if (timer === 0) {
+            localStorage.removeItem(localStorageKey);
+            clearInterval(interval);
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [localStorageKey, timer]);
+
+    function formattedTimer() {
+        const ONE_MINUTE_IN_SECONDS = 60;
+        const minutes = Math.floor(timer / ONE_MINUTE_IN_SECONDS);
+        const seconds = Math.floor(timer - minutes * 60);
+
+        const pad = (n: number) => (n < 10 ? `0${n}` : n);
+
+        return `${pad(minutes)}:${pad(seconds)}`;
+    }
+
+    function resetTimer() {
+        localStorage.removeItem(localStorageKey);
+        setTimer(startTime);
+    }
+
+    return {
+        timer,
+        formattedTimer: formattedTimer(),
+        resetTimer,
+    };
+}
