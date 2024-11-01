e5259f444d1a98aa6b03f9dbb9405e7deb1f0e94
Erick244 <erienrique03@gmail.com>
2024-11-01 17:33:55 -0300

    feat(Server:UserController): add user controller

---
+++ server/src/user/controllers/user.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test } from "@nestjs/testing";
+import { UserController } from "./user.controller";
+
+describe("UserController", () => {
+    let controller: UserController;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            controllers: [UserController],
+        }).compile();
+
+        controller = module.get(UserController);
+    });
+
+    it("should be defined", () => {
+        expect(controller).toBeDefined();
+    });
+});
