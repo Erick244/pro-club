7f7b69fd5111fab18443f4b416fd47bd2ff0b294
Erick244 <erienrique03@gmail.com>
2024-11-11 17:58:53 -0300

    feat(Client:AnimatedInput): add toggle password visibility

--- client/src/components/utils/forms/inputs/AnimatedInput.tsx
+++ client/src/components/utils/forms/inputs/AnimatedInput.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 import {
     FocusEvent,
     forwardRef,
     HTMLAttributes,
     HTMLInputTypeAttribute,
+    MouseEvent,
     Ref,
     useId,
     useState,
@@ -23,6 +26,9 @@ export const AnimatedInput = forwardRef(
     ({ label, ...props }: AnimatedInputProps, ref: Ref<any>) => {
         const inputId = useId();
         const [isFocused, setIsFocused] = useState<boolean>(!!props.value);
+        const [currentType, setCurrentType] = useState<HTMLInputTypeAttribute>(
+            props?.type || "text"
+        );
 
         function onFocus(e: FocusEvent<HTMLInputElement>) {
             props.onFocus ? props.onFocus(e) : null;
@@ -35,6 +41,16 @@ export const AnimatedInput = forwardRef(
 
             if (e.target.value.trim()) return;
             setIsFocused(false);
+        }
+
+        function togglePasswordVisibility(e: MouseEvent<HTMLButtonElement>) {
+            e.preventDefault();
+
+            if (currentType === "password") {
+                setCurrentType("text");
+            } else {
+                setCurrentType("password");
+            }
         }
 
         return (
@@ -51,14 +67,30 @@ export const AnimatedInput = forwardRef(
                 <Input
                     id={props.id || inputId}
                     {...props}
+                    type={currentType}
                     ref={ref}
                     onFocus={onFocus}
                     onBlur={onBlur}
                     className={cn(
-                        "focus-visible:border-primary bg-transparent border-b-2 border-t-0 border-l-0 border-r-0 border-foreground rounded-t rounded-b-none outline-none ring-0 ring-transparent focus-visible:ring-0 focus-visible:ring-transparent ring-offset-0 focus-visible:ring-offset-0 pl-1",
+                        "focus-visible:border-primary bg-transparent border-b-2 border-t-0 border-l-0 border-r-0 border-foreground rounded-t rounded-b-none outline-none ring-0 ring-transparent focus-visible:ring-0 focus-visible:ring-transparent ring-offset-0 focus-visible:ring-offset-0 pl-1 pr-7",
                         props.className
                     )}
                 />
+
+                {props.type === "password" && (
+                    <Button
+                        onClick={togglePasswordVisibility}
+                        variant="ghost"
+                        size="icon"
+                        className="absolute right-0 top-0 hover:bg-transparent"
+                    >
+                        {currentType === "password" ? (
+                            <EyeIcon className="w-5 h-5" />
+                        ) : (
+                            <EyeOffIcon className="w-5 h-5" />
+                        )}
+                    </Button>
+                )}
             </div>
         );
     }
