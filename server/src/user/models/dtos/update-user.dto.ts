8b6cdf64b124a8e0c6a977350e2b8e46b93dea68
Erick244 <erienrique03@gmail.com>
2024-11-13 18:00:20 -0300

    refactor(Server:UpdateUserDto): refactor UpdateUserDto
    - Removed properties that cannot be updated.
    - Added class-validator to validate each property.
    - Countries enum added.

--- server/src/user/models/dtos/update-user.dto.ts
+++ server/src/user/models/dtos/update-user.dto.ts
@@ -1,8 +1,22 @@
-import { Type } from "@nestjs/common";
-import { OmitType, PartialType } from "@nestjs/mapped-types";
-import { User } from "@prisma/client";
+import { IsEmail, IsEnum, IsOptional, Length } from "class-validator";
+import { Countries } from "../enums/countries.enum";
 
-export class UpdateUserDto extends OmitType(PartialType({} as Type<User>), [
-    "password",
-    "id",
-]) {}
+export class UpdateUserDto {
+    @IsOptional()
+    @Length(2, 50)
+    name?: string;
+
+    @IsOptional()
+    @IsEmail()
+    email?: string; // Temp
+
+    @IsOptional()
+    @Length(0, 256)
+    biography?: string | null;
+
+    @IsOptional()
+    @IsEnum(Countries, {
+        message: "add a valid country code. (br, us, cn...)",
+    })
+    country?: string | null;
+}
